148a469465de7769d5f0ed779c5db49104302769
Sayid Fatich Sholachuddin Alayyubi <sayidyubi28@gmail.com>
2022-09-01 11:38:42 +0800

    model reception

--- app/Models/reception.php
+++ app/Models/reception.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class reception extends Model
 {
     use HasFactory;
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+
+    protected $primaryKey = 'id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable =
+    ['nama', 'nik', 'foto_penerima', 'no_hp', 'jenkel', 'alamat', 'pekerjaan', 'penyakit', 'rt', 'foto_ktp', 'foto_kk',
+    'foto_rumah', 'status_rumah', 'long', 'lat'];
+
+    protected $guarded = ['id'];
 }
